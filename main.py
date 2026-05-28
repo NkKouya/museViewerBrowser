0032b24626971c5b615d0936db9d59ce301ec669
Yuya Kaneko <mckouya@gmail.com>
2026-05-28 14:13:40 +0900

    ブラウザ表示できるようにした

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import asyncio
 import websockets
 from pylsl import StreamInlet, resolve_streams
+import json
 
 async def stream_to_websocket(websocket):
-    streams = resolve_streams('type', 'EEG')
+    # EEG ストリームを探す
+    streams = resolve_streams(prop='type', value='EEG')
     inlet = StreamInlet(streams[0])
 
     while True:
-        sample, _ = inlet.pull_sample()
-        await websocket.send(str(sample))
+        sample, timestamp = inlet.pull_sample()
+        await websocket.send(json.dumps(sample))
 
 async def main():
     async with websockets.serve(stream_to_websocket, "localhost", 8765):
